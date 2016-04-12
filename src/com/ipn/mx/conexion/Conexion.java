e49db500cef0cb702ef880c773e078c0eb4ce961
Brenda <bdfe_23@hotmail.com>
2016-04-11 19:23:08 -0500

    modifiqueuieyr

--- src/com/ipn/mx/conexion/Conexion.java
+++ src/com/ipn/mx/conexion/Conexion.java
@@ -30,8 +30,7 @@ public class Conexion {
         return con;
     }
     
-    modipos chido 
- 
+    modipos
 
     Statement createStatement() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
